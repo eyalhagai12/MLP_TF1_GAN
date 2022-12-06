d99616fca7600de9f0ec6a5961bda7267819cb40
eyalhagai12 <eyalh2001@gmail.com>
2022-12-06 16:37:33 +0200

    added preprocess python file for preprocessing funtions

---
+++ preprocess.py
@@ -0,0 +1,29 @@
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+BW_IMAGES_PATH = os.path.join("samples", "bw")
+RGB_IMAGES_PATH = os.path.join("samples", "rgb")
+IMG_SHAPE = (128, 128)
+
+# if bw directory is empty,fill it
+if len(os.listdir(BW_IMAGES_PATH)) <= 0:
+    for image_name in os.listdir(RGB_IMAGES_PATH):
+        image = cv2.imread(os.path.join(RGB_IMAGES_PATH, image_name))
+        cv2.imwrite(os.path.join(BW_IMAGES_PATH, image_name), cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+
+
+def preprocess_image(image: np.ndarray) -> np.ndarray:
+    image = cv2.resize(image, IMG_SHAPE)
+    return image
+
+
+if __name__ == '__main__':
+    for image_name in os.listdir(RGB_IMAGES_PATH):
+        plt.cla()
+        image = cv2.imread(os.path.join(RGB_IMAGES_PATH, image_name))
+        image = preprocess_image(image)
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.pause(2)
